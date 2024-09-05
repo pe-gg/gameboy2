05fab1eededfbff626a607061c74f2ee12d46423
Garan <luigipeg12345@gmail.com>
2024-09-05 13:12:23 -0600

    Basic Setup Initialized
    Adds a scene with a character that moves

---
+++ MimicsDX/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+
+    [SerializeField] private float _movementSpeed = 5f;
+
+    public Vector2 movement;
+
+    [SerializeField] private float playerFriction;
+
+    [SerializeField] private float _xSpeed;
+
+    [SerializeField] private float _ySpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    public void HandleMovementInput(Vector2 input)
+    {
+        movement = input.normalized;
+    }
+
+    private void PlayerMovement()
+    {
+        //_rb.velocity = Vector2.Lerp(_rb.velocity, _movement.normalized * _movementSpeed * Time.fixedDeltaTime, playerFriction);
+        _rb.velocity = movement * (_movementSpeed * Time.fixedDeltaTime);
+    }
+}
