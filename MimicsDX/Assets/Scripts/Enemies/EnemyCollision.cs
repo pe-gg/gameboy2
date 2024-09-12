a246eb0c6947c831fd5d927534efe5c774b68bfc
Garan <luigipeg12345@gmail.com>
2024-09-12 12:05:30 -0600

    Enemy Movement Started, Knockback Added

--- MimicsDX/Assets/Scripts/Enemies/EnemyCollision.cs
+++ MimicsDX/Assets/Scripts/Enemies/EnemyCollision.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyCollision : BaseCollision
 {
+    [SerializeField] private float knockbackForce;
     private Rigidbody2D _rb;
+    private EnemyMovement _nav;
+    public Collider2D thisCol;
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponentInParent<Rigidbody2D>();
+        _nav = GetComponentInParent<EnemyMovement>();
+        thisCol = GetComponent<Collider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.rotation = Quaternion.identity;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,13 +27,15 @@ public class EnemyCollision : BaseCollision
 
     public void HandleCollision(BaseCollision col)
     {
+        thisCol.enabled = false;
         Debug.Log(col.gameObject.name);
         TakeKnockback(col);
     }
 
     private void TakeKnockback(BaseCollision collidedWith)
     {
+        _nav.currentState = EnemyMovement.EnemyState.PAIN;
         Vector3 direction = (collidedWith.transform.position - this.transform.position).normalized;
-        _rb.AddForce(2f * -direction, ForceMode2D.Impulse);
+        _rb.AddForce(knockbackForce * -direction, ForceMode2D.Impulse);
     }
 }
--- MimicsDX/Assets/Scripts/Enemies/EnemyMovement.cs
+++ MimicsDX/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -1,24 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] private GameObject[] _patrolPoints;
-    [SerializeField] private float _acceptanceRadius;
+    [SerializeField] private GameObject[] patrolPoints;
+    [SerializeField] private float acceptanceRadius;
+    [SerializeField] private GameObject target;
+    [SerializeField] private int painDuration;
+    private Rigidbody2D _rb;
+    private int defaultPainDuration;
+    private NavMeshAgent _agent;
     private int _currentPoint;
-    private enum EnemyState
+    public EnemyState currentState;
+    private SpriteRenderer _spr;
+    private EnemyCollision _col;
+    private bool _inPain;
+    private bool _inPain2;
+    public enum EnemyState
     {
         PATROL,
-        FOLLOW
+        FOLLOW,
+        PAIN
     }
     private void Awake()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
+        _spr = GetComponentInChildren<SpriteRenderer>();
+        _col = GetComponentInChildren<EnemyCollision>();
+        _rb = GetComponent<Rigidbody2D>();
+        currentState = EnemyState.FOLLOW;
+        defaultPainDuration = painDuration;
     }
     private void FixedUpdate()
     {
-        
+        FixRotations();
+        switch (currentState)
+        {
+            case EnemyState.PATROL:
+                Patrol();
+                return;
+            case EnemyState.FOLLOW:
+                Follow();
+                return;
+            case EnemyState.PAIN:
+                if (_inPain)
+                    return;
+                StartPain();
+                return;
+        }
     }
 
+    private void Patrol()
+    {
+
+    }
+
+    private void Follow()
+    {
+        _agent.SetDestination(target.transform.position);
+    }
+
+    private void StartPain()
+    {
+        _inPain = true;
+        _inPain2 = true;
+        StartCoroutine("Pain");
+    }
+
+    IEnumerator Pain()
+    {
+        Debug.Log("pain");
+        while (_inPain2)
+        {
+            _agent.velocity = _rb.velocity;
+            painDuration--;
+            if (painDuration <= 0) 
+                _inPain2 = false;
+            yield return new WaitForFixedUpdate();
+        }
+        Debug.Log("out of pain");
+        painDuration = defaultPainDuration;
+        currentState = EnemyState.FOLLOW;
+        _rb.velocity = Vector2.zero;
+        _col.thisCol.enabled = true;
+        _inPain = false;
+        yield return new WaitForFixedUpdate();
+    }
+
+    private void FixRotations()
+    {
+        _spr.gameObject.transform.rotation = Quaternion.identity;
+        _spr.gameObject.transform.position = this.transform.position;
+    }
 }
